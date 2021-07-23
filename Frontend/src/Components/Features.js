7f79598d756cb6444ef79445fb441aff14602ce6
Sanket Salunke <salunkess2001@gmail.com>
2021-07-23 20:15:03 +0530

    Layout Changed

--- Frontend/src/Components/Features.js
+++ Frontend/src/Components/Features.js
@@ -1,41 +1,47 @@
-import React from 'react';
-import { features } from '../Data';
-import { Col, Container, Row } from 'react-bootstrap';
-import './Features.css';
+import React from "react";
+import { features } from "../Data";
+import { Col, Container, Row } from "react-bootstrap";
+import "./Features.css";
 const Features = () => {
   return (
     <div>
       <center>
-        <h3 style={{ color: '#000004', fontWeight: 600 }} className='mt-4'>
+        <h3
+          style={{
+            color: "#5f43bd",
+            fontWeight: 600,
+            fontSize: `40px`,
+            marginTop: `45px`,
+          }}
+        >
           Features
         </h3>
-          </center>
-          <div id="Home"></div>
+      </center>
+      <div id="Features"></div>
       <Container className="mb-4">
-        <div className='flex flex-wrap -m-4'>
-          <Row className='ml-3 mt-4'>
-            {features.map((features) => (
-              <Col
-                className='card border-secondary mt-4'
-                key={features.image}
+        <div className="flex flex-wrap -m-4">
+          <div className="row ml-3 mt-4">
+            {features.map((features, index) => (
+              <div
+                className="col-sm card border-secondary mt-4 col-xl"
+                key={index}
                 style={{
                   boxShadow: `-5px -5px #000`,
                   borderRadius: 10,
-                    border: `2px solid #000`,
-                    marginLeft: 15,
-                    marginRight:15,
-                    width: 350
-                }}>
-                <h5 className='text-center'>{features.title}</h5>
-                <img
-                  alt='gallery'
-                  className='absolute inset-0 w-full h-full object-cover object-center'
-                  src={features.image}
-                />
-                <p className='leading-relaxed p-2'>{features.description}</p>
-              </Col>
+                  border: `2px solid #000`,
+                  marginLeft: 15,
+                  marginRight: 15,
+                  width: 350,
+                }}
+              >
+                <h5 className="text-center mt-3 mb-2">{features.title}</h5>
+                <img alt="gallery" src={features.image} />
+                <p className="leading-relaxed p-2 mt-2">
+                  {features.description}
+                </p>
+              </div>
             ))}
-          </Row>
+          </div>
         </div>
       </Container>
     </div>
--- Frontend/src/Components/Navbar.js
+++ Frontend/src/Components/Navbar.js
@@ -2,17 +2,74 @@ import React from "react";
 import "./Navbar.css";
 
 const Navbar = () => {
+  document.addEventListener("scroll", () => {
+    if (window.pageYOffset > 580) {
+    } else {
+    }
+    if (window.pageYOffset > 5) {
+      document.getElementById("navbar").style.backgroundColor = "#f9f9f9";
+
+      document.getElementById("navbar").style.boxShadow = `0px 2px 6px #5f43bd`;
+
+      document.getElementById("c").style.color = "#000";
+
+      document.getElementById("nav-ele1").style.color = "#000";
+      document.getElementById("nav-ele2").style.color = "#000";
+      document.getElementById("nav-ele3").style.color = "#000";
+      document.getElementById("nav-ele4").style.color = "#000";
+
+      document.getElementById("nav-ele5").style.backgroundColor = "#5f43bd";
+      document.getElementById("nav-ele5").style.color = "#fff";
+    } else {
+      document.getElementById("navbar").style.backgroundColor =
+        "rgba(0, 0, 0, 0.09)";
+
+      document.getElementById("c").style.color = "#fff";
+
+      document.getElementById("navbar").style.boxShadow = null;
+
+      document.getElementById("nav-ele1").style.color = "#fff";
+      document.getElementById("nav-ele2").style.color = "#fff";
+      document.getElementById("nav-ele3").style.color = "#fff";
+      document.getElementById("nav-ele4").style.color = "#fff";
+
+      document.getElementById("nav-ele5").style.backgroundColor = "#fff";
+      document.getElementById("nav-ele5").style.color = "#000";
+    }
+  });
+
   return (
     <div>
-      <nav className="navbar">
-        <span class="header-text">Student Portal</span>
+      <a href="Home" />
+      <nav id="navbar" className="navbar">
+        <span className="header-text" id="c">
+          Student Portal
+        </span>
         <ul className="options">
-          <li className="ind-options"><a href="#Home">Home</a></li>
-          <li className="ind-options">About</li>
-          <li className="ind-options">Features</li>
-          <li className="ind-options">Contact Us</li>
           <li className="ind-options">
-            <button className="signup_btn">Sign Up</button>
+            <a id="nav-ele1" href="#Home">
+              Home
+            </a>
+          </li>
+          <li className="ind-options">
+            <a id="nav-ele2" href="#Home">
+              About
+            </a>
+          </li>
+          <li className="ind-options">
+            <a id="nav-ele3" href="#Features">
+              Features
+            </a>
+          </li>
+          <li className="ind-options">
+            <a id="nav-ele4" href="#Home">
+              Contact Us
+            </a>
+          </li>
+          <li className="ind-options">
+            <button id="nav-ele5" className="signup_btn">
+              Sign Up
+            </button>
           </li>
         </ul>
       </nav>
