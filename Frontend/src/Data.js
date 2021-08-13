5320752a65837fa1058cbaed9a3bd322d90cd872
Sanket Salunke <salunkess2001@gmail.com>
2021-08-13 19:48:32 +0530

    Files Added

---
+++ Frontend/src/Data.js
@@ -0,0 +1,25 @@
+export const features = [
+  {
+    title: 'Submit Assignments Easily',
+    description:
+      'Opportunity to learn as you work on real world technology problem.',
+    image: './Images/Teacher2.svg'
+  },
+  {
+    title: 'Interaction with fellow Students',
+    description:
+      'Learn best practices under the guidance of technology leaders from the industry.',
+    image: './Images/Teacher.svg'
+  },
+  {
+    title: 'Easy for Teachers to track other students',
+    description: 'Live projects to showcase on your resume.',
+    image: './Images/Teacher1.svg'
+  },
+  {
+    title: 'Students can Join classes easily',
+    description:
+      'Learn best practices under the guidance of technology leaders from the industry.',
+    image: './Images/Teacher2.svg'
+  }
+];
